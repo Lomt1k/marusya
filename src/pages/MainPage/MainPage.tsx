2d877e04e0612edcbb7b91335f5152a090ac23ca
Vladimir Yashin <yashin2236@gmail.com>
2025-03-09 17:14:23 +0300

    Компонент Container

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,5 @@
 import Button from '../../components/ui/Button/Button';
+import Container from '../../components/ui/Container/Container';
 import Input from '../../components/ui/Input/Input';
 import Rating from '../../components/ui/Rating/Rating';
 import TextButton from '../../components/ui/TextButton/TextButton';
@@ -6,17 +7,19 @@ import TextButton from '../../components/ui/TextButton/TextButton';
 const MainPage = () => {
   return (
     <main>
-      <h1>Main Page</h1>
-      <p>Welcome to the main page</p>
-      <Button onClick={() => {}}>О фильме</Button>
-      <Button onClick={() => {}} secondary>О фильме</Button>
-      <Input type='email' name='test' placeholder='Электронная почта' icon={<div>XX</div>} />
-      <Input type='email' name='test' placeholder='Электронная почта' icon={<div>XX</div>} dark />
-      <TextButton onClick={() => {}}>Регистрация</TextButton>
-      <Rating rating={10}/>
-      <Rating rating={7.5}/>
-      <Rating rating={6.3}/>
-      <Rating rating={4.2}/>
+      <Container>
+        <h1>Main Page</h1>
+        <p>Welcome to the main page</p>
+        <Button onClick={() => {}}>О фильме</Button>
+        <Button onClick={() => {}} secondary>О фильме</Button>
+        <Input type='email' name='test' placeholder='Электронная почта' icon={<div>XX</div>} />
+        <Input type='email' name='test' placeholder='Электронная почта' icon={<div>XX</div>} dark />
+        <TextButton onClick={() => {}}>Регистрация</TextButton>
+        <Rating rating={10}/>
+        <Rating rating={7.5}/>
+        <Rating rating={6.3}/>
+        <Rating rating={4.2}/>
+      </Container>
     </main>
   )
 }
