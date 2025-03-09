896213f2d9ffb0dbedb84c353a8a032e1410a512
Vladimir Yashin <yashin2236@gmail.com>
2025-03-09 17:01:02 +0300

    Компонент Rating

---
+++ src/components/ui/Rating/Rating.tsx
@@ -0,0 +1,26 @@
+import './Rating.scss';
+import StarIcon from '/src/assets/icons/star.svg?react';
+import { FC } from 'react';
+
+type RatingProps = {
+  rating: number;
+  small?: boolean;
+}
+
+const getRatingModifier = (rating: number) => {
+  if (rating >= 8.5) return '';
+  if (rating >= 7) return ' rating--good';
+  if (rating >= 5) return ' rating--normal';
+  return ' rating--bad';
+}
+
+const Rating: FC<RatingProps> = ({ rating, small }) => {
+  return (
+    <div className={'rating' + (small ? ' rating--s' : '') + getRatingModifier(rating)}>
+      <StarIcon />
+      <span>{rating.toLocaleString()}</span>
+    </div>
+  )
+}
+
+export default Rating;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 import Button from '../../components/ui/Button/Button';
 import Input from '../../components/ui/Input/Input';
+import Rating from '../../components/ui/Rating/Rating';
 import TextButton from '../../components/ui/TextButton/TextButton';
 
 const MainPage = () => {
@@ -12,6 +13,10 @@ const MainPage = () => {
       <Input type='email' name='test' placeholder='Электронная почта' icon={<div>XX</div>} />
       <Input type='email' name='test' placeholder='Электронная почта' icon={<div>XX</div>} dark />
       <TextButton onClick={() => {}}>Регистрация</TextButton>
+      <Rating rating={10}/>
+      <Rating rating={7.5}/>
+      <Rating rating={6.3}/>
+      <Rating rating={4.2}/>
     </main>
   )
 }
