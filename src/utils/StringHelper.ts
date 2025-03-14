73f44aedba05ac9f534e031a977f4a7e14fc0d0d
Vladimir Yashin <yashin2236@gmail.com>
2025-03-14 10:45:09 +0300

    Доработки по методу truncateText в StringHelper

--- src/utils/StringHelper.ts
+++ src/utils/StringHelper.ts
@@ -1,12 +1,15 @@
 export default class StringHelper {
   public static truncateText(text: string, digits: number) {
-    if (text.length < digits) {
+    if (text.length <= digits) {
       return text;
     }
   
-    return text
-      .substring(0, digits)
-      .replace(/[\s.,;!?()]+$/, '')
-      + '...';
+    text = text.substring(0, digits);
+    // Удаляем последнее слово, т.к. при обрезании через substring могли обрезать на половине слова
+    return StringHelper.removeLastWord(text) + '...';
+  }
+
+  public static removeLastWord(text: string) {
+    return text.replace(/\s+\S+$/, '');
   }
 }
