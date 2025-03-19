45ab2f5ae2290976d17b4df79bd6fc047b2f4dee
Vladimir Yashin <yashin2236@gmail.com>
2025-03-19 17:22:10 +0300

    Окно авторизации (доработки по вёрстке)

--- src/components/Modals/AuthModal/FinishRegisterModal.tsx
+++ src/components/Modals/AuthModal/FinishRegisterModal.tsx
@@ -1,7 +1,10 @@
 import { FC } from "react";
 import RootStore from "../../../store/RootStore";
 import Modal from "../Modal";
-import './FinishRegisterModal.scss';
+import Button from "../../ui/Button/Button";
+import Heading from "../../ui/Heading/Heading";
+import LogoDark from "../../ui/LogoDark/LogoDark";
+import './AuthModal.scss';
 
 type FinishRegisterModalProps = {
   onClickLogin: Function;
@@ -9,8 +12,13 @@ type FinishRegisterModalProps = {
 
 const FinishRegisterModal: FC<FinishRegisterModalProps> = ({ onClickLogin }) => {
   return (
-    <Modal onClickClose={() => RootStore.auth.setModalActive(false)}>
-      FINISH
+    <Modal className="auth-modal" onClickClose={() => RootStore.auth.setModalActive(false)}>
+      <LogoDark className="auth-modal__logo" />
+      <div className="auth-modal__content">
+        <Heading dark level={4} className="auth-modal__heading">Регистрация завершена</Heading>
+        <p className="auth-modal__text">Используйте вашу электронную почту для входа</p>
+        <Button onClick={() => onClickLogin()}>Войти</Button>\
+      </div>
     </Modal>
   )
 }
--- src/components/Modals/AuthModal/LoginModal.tsx
+++ src/components/Modals/AuthModal/LoginModal.tsx
@@ -1,13 +1,13 @@
-import { FC, useEffect, useRef } from "react";
+import { FC } from "react";
 import RootStore from "../../../store/RootStore";
 import Modal from "../Modal";
 import LogoDark from "../../ui/LogoDark/LogoDark";
 import Input from "../../ui/Input/Input";
 import IconEmail from '/src/assets/icons/email.svg?react';
 import IconPassword from '/src/assets/icons/password.svg?react';
-import './LoginModal.scss';
 import Button from "../../ui/Button/Button";
 import ButtonText from "../../ui/ButtonText/ButtonText";
+import './AuthModal.scss';
 
 type LoginModalProps = {
   onClickRegister: Function;
@@ -16,18 +16,13 @@ type LoginModalProps = {
 const LoginModal: FC<LoginModalProps> = ({ onClickRegister }) => {
   const iconEmail = <IconEmail aria-hidden={true} width={24} height={24} />
   const iconPassword = <IconPassword aria-hidden={true} width={24} height={24} />
-  const firstInputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    setTimeout(() => firstInputRef.current?.focus(), 0);
-  }, []);
 
   return (
-    <Modal className="login-modal" onClickClose={() => RootStore.auth.setModalActive(false)}>
-      <LogoDark className="login-modal__logo" />
-      <div className="login-modal__content">
-        <div className="login-modal__inputs">
-          <Input type='email' name='email' placeholder='Электронная почта' icon={iconEmail} ref={firstInputRef} />
+    <Modal className="auth-modal" onClickClose={() => RootStore.auth.setModalActive(false)}>
+      <LogoDark className="auth-modal__logo" />
+      <div className="auth-modal__content">
+        <div className="auth-modal__inputs">
+          <Input type='email' name='email' placeholder='Электронная почта' icon={iconEmail} />
           <Input type='password' name='password' placeholder='Пароль' icon={iconPassword} />
         </div>
         <Button onClick={() => {}}>Войти</Button>
