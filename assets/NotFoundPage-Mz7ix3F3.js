d3a791f4f8c5bd91ca3cf736dfe8c83bd1049b51
Vladimir Yashin <yashin2236@gmail.com>
2025-03-21 19:03:22 +0300

    Внёс еще небольшие правки

---
+++ assets/NotFoundPage-Mz7ix3F3.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CnMlcSHu.js";import{N as o}from"./NotFound-BmyBShf6.js";import"./Heading-CAVCVwa6.js";import"./ButtonLink-BsF9jzos.js";const e=()=>t.jsx("main",{children:t.jsx(o,{title:"Страница не найдена"})});export{e as default};
